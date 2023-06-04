2f60462881ca814876ff56b27de89764f92c3ab7
Caleb Mackey <caleb.mackey@gmail.com>
2023-06-04 12:30:36 -0500

    feat(egg-guy): add winning animation and states

--- src/components/EggGuy.vue
+++ src/components/EggGuy.vue
@@ -1,57 +1,121 @@
 <script lang="ts" setup>
 import { MouthState, LegState } from '@/types'
-import { ref, computed, onMounted } from 'vue'
+import { ref, computed, onMounted, watch } from 'vue'
+import { gifAnimationDuration, delay, log } from '@/utils'
 import { useDropZone } from '@vueuse/core'
 
 const emit = defineEmits<{
-  dropEgg: []
+  'drop-egg': [];
+  'started-winning-celebration': [];
+  'started-walking': [];
+  'stopped-waling': [];
+  'showing-bush-bare-back-balls-and-butt': [];
+  'showing-butt-frame': [];
+  'ended-winning-celebration': [];
 }>()
-
 
 interface Props {
   legState: LegState;
-  eating: boolean;
+  eating?: boolean;
+  celebrate?: boolean;
 }
 
+// defaults, but dynamically grab them on mounted
+const walkingTimeout = ref(5430)
+const nudeTimeout = ref(8050)
+const shouldWalk = ref(false)
+const showEmNude = ref(false)
+const showButtFrame = ref(false)
 const dropZoneRef = ref()
-const { legState='Down', eating=false } = defineProps<Props>()
+const { legState='Down', eating=false, celebrate=false } = defineProps<Props>()
 
 const mouthState = computed<MouthState>(()=> eating ? 'Closed': 'Open')
 
-const imageSource = computed(()=> new URL(`../assets/images/${mouthState.value}-Mouth-Egg-${legState}.png`, import.meta.url).href)
+const walkingGif = new URL(`../assets/images/egg-walking.gif`, import.meta.url).href
+const nudeGif = new URL(`../assets/images/bare-back-balls-butt.gif`, import.meta.url).href
+const eggButtFrame = new URL(`../assets/images/egg-butt-frame.png`, import.meta.url).href
+
+const imageSource = computed(()=> celebrate 
+  ? shouldWalk.value  
+    ? walkingGif
+    : showEmNude.value 
+      ? nudeGif
+      : eggButtFrame 
+  : new URL(`../assets/images/${mouthState.value}-Mouth-Egg-${legState}.png`, import.meta.url).href
+)
 
 const onDrop = (evt: any)=> {
-  console.log('on drop: ', evt)
-  emit('dropEgg')
+  log('dropped egg: ', evt)
+  emit('drop-egg')
 }
 
-const onDragover = (evt: any)=> {
-  // console.log('on dragover: ', evt)
-  // emit('dropEgg')
+const showBareBackButtAndBalls = async ()=> {
+  // start celebration
+  emit('started-winning-celebration')
+
+  // start walking
+  shouldWalk.value = true
+  emit('started-walking')
+  await delay(walkingTimeout.value)
+  shouldWalk.value = false
+  showEmNude.value = true
+  emit('stopped-waling')
+
+  // show em nude
+  emit('showing-bush-bare-back-balls-and-butt')
+  await delay(nudeTimeout.value)
+  showButtFrame.value = true
+  showEmNude.value = false
+
+  // show still butt frame for 3 seconds
+  emit('showing-butt-frame')
+  await delay(5000)
+
+  // end celebration
+  emit('ended-winning-celebration')
 }
 
+watch(()=> celebrate, 
+  (shouldCelebrate) => {
+    log('should celebrate: ', shouldCelebrate)
+    if (shouldCelebrate){
+      showBareBackButtAndBalls()
+    }
+  }
+)
 
 onMounted(()=> {
-  const { isOverDropZone } = useDropZone(dropZoneRef.value, onDrop)
-  console.log('is over drop zone?', isOverDropZone)
+  useDropZone(dropZoneRef.value, onDrop)
+  gifAnimationDuration(walkingGif).then(d => { 
+    walkingTimeout.value = (d * 1000) - 50 
+    log(`set walking gif timeout: ${d}`)
+  })
+  gifAnimationDuration(nudeGif).then(d => {
+    nudeTimeout.value = (d * 1000) - 50
+    log(`set nude gif timeout: ${d}`)
+  })
   
 })
-
-
 
 </script>
 
 <template>
-  
   <div 
-    :class="{'egg-guy-down': legState === 'Down', 'egg-guy-chewing': eating, 'egg-guy': legState === 'Up' && !eating }" 
-    
+    :class="celebrate ? 'egg-guy' :
+      {
+        'egg-guy-down': legState === 'Down', 
+        'egg-guy-chewing': eating, 
+        'egg-guy': legState === 'Up' && !eating 
+      }" 
   >
-    <div class="egg-guy-img-wrapper" ref="dropZoneRef">
-      <img :src="imageSource" alt="">
-      <!-- <img src="" alt="" v-if="shouldEat" class="eating"> -->
+    <div class="egg-guy-img-wrapper" ref="dropZoneRef" @dragover.prevent>
+      <img :src="imageSource" alt="" :class="celebrate ? 'walking-egg': ''">
     </div>
   </div>
-
-
 </template>
+
+<style>
+.egg-guy-img-wrapper:hover {
+  cursor: url('./Tiny-Cursor.png'), pointer !important;
+}
+</style>
